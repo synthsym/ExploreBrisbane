5c6ac8bffa47803dc6fa7433f86cc83fc6c31df4
Adam <snoopyk921@gmail.com>
2012-05-01 11:58:22 +1000

    Pre-built Rails Code
    Signed-off-by: Adam <snoopyk921@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Git::Application.config.secret_token = 'c0af6e1bce16251bec46051e74b1fe440dc6db2c9b0a5ac1e6305479b87a679aa9ebc539713c4e4970bee90a2882213cdb6a97ff8c5d77a7dc92918c3ffd1010'
